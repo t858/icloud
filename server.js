87e8cc436e788e8ed3fbba479119db89a80fe0b4
Timmy <69387293+t858@users.noreply.github.com>
2026-08-26 00:08:09 +0100

    Add Render Web Service startup and proxy streaming

--- server.js
+++ server.js
@@ -209,10 +209,12 @@ const requestHandler = (req, res) => {
 
         res.writeHead(200, {
             'Content-Type': 'text/event-stream',
-            'Cache-Control': 'no-cache',
-            'Connection': 'keep-alive'
+            'Cache-Control': 'no-cache, no-transform',
+            'Connection': 'keep-alive',
+            'X-Accel-Buffering': 'no',
+            'Access-Control-Allow-Origin': '*'
         });
-        res.write('\n');
+        res.write(': keepalive\n\n');
 
         if (!sseClients.has(orderId)) {
             sseClients.set(orderId, []);
